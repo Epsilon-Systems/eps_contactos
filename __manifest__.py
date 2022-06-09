9b0f75017aa13572e81351073b44205552d3f185
AnibalArenas <anibalarenas2107@gmail.com>
2022-06-09 10:01:11 -0500

    [UPD] KYC fields

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "epsy_contacto",
+    'name': "KYC",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Know Your Customer Module""",
 
     'description': """
-        Long description of module's purpose
+        Module that adds fields to the contact form for PLD and FT in the financial sector
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Epsilon Systems",
+    'website': "http://www.epsilonsystemsgroup.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Administration',
+    'version': '15.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -32,4 +31,6 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'application': True,
+    'installable': True,
 }
--- models/models.py
+++ models/models.py
@@ -1,18 +1,35 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import api, fields, models
+from datetime import datetime, timedelta, time
+from dateutil.relativedelta import relativedelta
+from odoo.exceptions import UserError, ValidationError, Warning
+import urllib.parse
+import requests
+import math
 
 
-# class epsy_contacto(models.Model):
-#     _name = 'epsy_contacto.epsy_contacto'
-#     _description = 'epsy_contacto.epsy_contacto'
+class ResPartner(models.Model):
+    _inherit = "res.partner"
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    curp = fields.Char(string='CURP', help='Clave Única de Registro de Población')
+    id_no = fields.Char(string='INE', help="INE o Número de identificación")
+    ocupation = fields.Many2one(comodel_name="res.partner.industry",
+                                string="Actividad",
+                                help="Ocupación, Profesión, Actividad ó giro del negocio al que se dedique el cliente")
+    foreign = fields.Selection([('res', 'Reside en el extranjero'),
+                                ('nac', 'Nacido en el extranjero'),
+                                ('amb', 'Nació y reside en el extranjero'),],
+                               string="Extranjero", help='Establece si una persona vive o nació en el extranjero')
+    #Validación de documentos
+    chk_vat = fields.Boolean(string='RFC')
+    chk_cur = fields.Boolean(string='RFC')
+    chk_ids = fields.Boolean(string='INE o Pasaporte')
+    chk_act = fields.Boolean(string='Acta Constitutiva')
+    chk_avi = fields.Boolean(string='Aviso de Incripción')
+    chk_est = fields.Boolean(string='Estructura Accionaria')
+    chk_org = fields.Boolean(string='Organigrama')
+
+    #Validación
+    chk_gob = fields.Boolean(string='Gobierno')
+    chk_fid = fields.Boolean(string='Fideicomiso')
